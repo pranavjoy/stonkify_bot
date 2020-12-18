e47fa02c9309116e482a2bbc9b9361bf6c32d284
Pranav <pranav@lodgify.com>
2020-12-18 11:00:34 +0100

    Fixed image url generation

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
 from PIL import Image
 
+import string
 import requests
 import random
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
+    CallbackContext
 import os
+import shutil
 
 logger = logging.getLogger(__name__)
 
-BASE_URL = "https://hardboiled.tech/"
+BASE_URL = ""
 
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 print(TOKEN)
 
 
-def image_url_generator():
-    image_id = random.randint(1000000, 1000000000)
-    image_url = "http://" + BASE_URL + "/images/" + str(image_id) + ".jpg"
-    resp = requests.get(image_url)
+def move(src, dest):
+    shutil.move(src, dest)
 
+
+def image_url_generator():
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(16))
+    image_url = "http://{}/images/{}.jpg".format(BASE_URL, result_str)
+    resp = requests.get(image_url)
     if resp.status_code == 200:
         image_url_generator()
     else:
-        return [image_url, image_id]
-
-
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Enter /stonkify <concept> to start generating memes')
-
-
-def stonkify(update: Update, context: CallbackContext) -> None:
-    #Search for the images on stock website merge images and return the merged image
-
-    stonk_base = str(context.args[0])
-    print(stonk_base)
-    update.message.reply_photo('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg')
+        return image_url, result_str
 
 
 def merge_two_images(image1_location, image2_location):
@@ -51,6 +40,33 @@ def merge_two_images(image1_location, image2_location):
     image2_size = image2.size
     image2.paste(image1, (0, 0), image1)
     image2.show()
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text(
+        'Enter /stonkify <concept> to start generating memes')
+
+
+def stock_photo(image_name):
+    # search for image, download it and return image location
+    image_location = "image2.jpeg"
+    return (image_location)
+
+
+def stonkify(update: Update, context: CallbackContext) -> None:
+    #Search for the images on stock website merge images and return the merged image
+    stonk_base = str(context.args[0])
+    image2_location = stock_photo(stonk_base)
+    merge_two_images("image1.png", image2_location)
+    url, image_id = image_url_generator()
+    move(image2_location, "images/" + image_id)
+    update.message.reply_photo(url)
 
 
 def main():
@@ -67,7 +83,6 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("stonkify", stonkify))
-
 
     # Start the Bot
     updater.start_polling()
